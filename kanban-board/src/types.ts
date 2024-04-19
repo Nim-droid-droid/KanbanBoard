02790e30c2ff79a1f11f57619f87b13c63b033ef
Nim-droid-droid <levkoz2222@outlook.com>
2024-04-19 21:29:56 +0100

    File modified

--- kanban-board/src/types.ts
+++ kanban-board/src/types.ts
@@ -1,3 +1,13 @@
+/*
+Use .ts for pure TypeScript files.
+
+Use .tsx for files which contain JSX.
+
+For example, a React component would be .tsx, but a file containing helper functions would be .ts.
+Just like you were doing React without TypeScript, you would use .js and .jsx for pure javascript vs JSX javascript files.
+*/
+// tsx extension is used when we want to embed JSX elements inside the files while .ts is used for plain Typescript files and do not support adding JSX Elements.
+
 export type Id = string | number;
 
 export type Column = {
