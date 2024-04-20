7da6019c6f4a14f031650271da969b2abdd0268c
Nim-droid-droid <levkoz2222@outlook.com>
2024-04-20 23:21:59 +0100

    File modified

--- kanban-board/src/types.ts
+++ kanban-board/src/types.ts
@@ -8,6 +8,17 @@ Just like you were doing React without TypeScript, you would use .js and .jsx fo
 */
 // tsx extension is used when we want to embed JSX elements inside the files while .ts is used for plain Typescript files and do not support adding JSX Elements.
 
+/*
++ Interfaces VS Types
+- Interfaces create a new, distinct type whereas a type alias is just that: an alias.
+
+- Both can be used to describe the shape of an object or a function signature. But the syntax differs. 
+(So theyre both like classes in JS which are blueprints to create obj from??)
+(Interfaces have shape of an Dictionary/JS Object, so non primitive types etc…)
+
+- Unlike an interface, the type alias can also be used for other types such as primitives, unions, and tuples.
+*/ 
+
 export type Id = string | number;
 
 export type Column = {
