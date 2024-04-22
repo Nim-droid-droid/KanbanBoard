ac794b8ad893967cb26f912290c0b0ffe89d5536
Nim-droid-droid <levkoz2222@outlook.com>
2024-04-22 22:58:56 +0100

    Props Interface initialised

--- kanban-board/src/components/ColumnContainer.tsx
+++ kanban-board/src/components/ColumnContainer.tsx
@@ -1,3 +1,12 @@
+import { Column, Id } from "../types"
+
+// interface Props so that i can defines the properties that are going to pass through the  column container, so  contains a column of type column
+interface Props{
+  column: Column;
+  // the function takes an ID of type ID nothing
+  deleteColumn: (id: Id) => void;
+}
+
 import React from 'react'
 
 function ColumnContainer() {
