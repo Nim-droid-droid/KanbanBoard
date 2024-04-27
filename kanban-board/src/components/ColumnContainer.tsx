1b32a80a5064b4481f6eb2ee7df27f4ceac47631
Nim-droid-droid <levkoz2222@outlook.com>
2024-04-27 21:56:30 +0100

    styled the title column

--- kanban-board/src/components/ColumnContainer.tsx
+++ kanban-board/src/components/ColumnContainer.tsx
@@ -16,9 +16,11 @@ function ColumnContainer(props: Props) {
     <div className="bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
 
       {/* column title */}
-      <div>
+      <div className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between">
 
-      </div>
+        <div className="flex gap-2">
+
+        </div>
 
       {/* column task container */}
       <div>Content</div>
