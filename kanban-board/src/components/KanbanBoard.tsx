acaa8bb9836c43e39c444edea4ff356cc42778a7
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-05 23:24:06 +0100

    import files

--- kanban-board/src/components/KanbanBoard.tsx
+++ kanban-board/src/components/KanbanBoard.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+// import React from 'react'
+
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
 
 function KanbanBoard() {
   return (
