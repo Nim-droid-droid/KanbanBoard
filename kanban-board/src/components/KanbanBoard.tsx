90df6f077269ad03fa2c817c21bc862e2f7e0d78
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-07 23:32:47 +0100

    File modified

--- kanban-board/src/components/KanbanBoard.tsx
+++ kanban-board/src/components/KanbanBoard.tsx
@@ -6,14 +6,15 @@ import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
 
 function KanbanBoard() {
+  // Anything that is passed into columns has to match the shape of type Column interface -> so whatever is passed into columns var has to be an obj that HAS data type str or num that will be stored into the id prop & a str that can be stored into the title prop of the of Column obj??
+    // This obj is then saved to useState columns
   const [columns, setColumns] = useState<Column[]>([]);
 
   return (
     <div>
-      
+
     </div>
-  )
+    )
 }
 
 export default KanbanBoard
-
